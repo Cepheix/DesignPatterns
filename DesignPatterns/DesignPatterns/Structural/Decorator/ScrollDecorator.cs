d8917f54e7a4e07c1b80e9fc70a0b66d4a0e42d3
Piotr Ptak <piotrptak@outlook.com>
2017-12-17 12:33:39 +0100

    added decorator example

---
+++ DesignPatterns/DesignPatterns/Structural/Decorator/ScrollDecorator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Decorator
+{
+    public class ScrollDecorator : Decorator
+    {
+        public ScrollDecorator(VisualComponent component) : base(component)
+        {
+        }
+
+        public ScrollDecorator(VisualComponent component, int scrollPosition) : this(component)
+        {
+            this.ScrollPosition = scrollPosition;
+        }
+
+        public override void Decorate()
+        {
+            ScrollTo(ScrollPosition);
+        }
+
+        private void ScrollTo(int scrollPosition)
+        {
+
+        }
+
+        public int ScrollPosition { get; set; }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Decorator/Window.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Decorator
+{
+    public class Window
+    {
+        public void DrawWindow()
+        {
+            VisualComponent textView = new TextView();
+
+            Decorator scroll = new ScrollDecorator(textView, 0); // add scroll
+
+            Decorator border = new BorderDecorator(scroll, 10); // add border
+
+            border.Draw(); // draw everything
+        }
+    }
+}
